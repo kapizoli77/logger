8d5bd3d1256cf4ef368c10f6f0bba59cd63a211b
Kapi Zoltán <zoltan.kapi@supercharge.io>
2023-10-29 22:36:09 +0100

    feat: add spm support

---
+++ Sources/Logger/Filter/Filter.swift
@@ -0,0 +1,16 @@
+//
+//  Filter.swift
+//  Logger
+//
+
+public protocol Filter: AnyObject {
+    var name: String { get }
+
+    func shouldLog(details: LogDetails) -> Bool
+}
+
+extension Filter {
+    public var name: String {
+        String(describing: Self.self)
+    }
+}

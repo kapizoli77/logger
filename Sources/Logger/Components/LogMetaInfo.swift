fc725bee9b922b501c06ebc41c95dc10f93517ee
Kapi Zoltán <zoltan.kapi@supercharge.io>
2023-10-29 22:36:09 +0100

    feat: add spm support

---
+++ Sources/Logger/Components/LogMetaInfo.swift
@@ -0,0 +1,20 @@
+
+//  LogDetails.swift
+//  Logger
+//
+
+import Foundation
+
+public struct LogMetaInfo {
+    public var date: Date
+    public var fileName: String
+    public var functionName: String
+    public var lineNumber: Int
+
+    init(date: Date, fileName: String, functionName: String, lineNumber: Int) {
+        self.date = date
+        self.fileName = fileName
+        self.functionName = functionName
+        self.lineNumber = lineNumber
+    }
+}
